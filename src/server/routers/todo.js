52bc968cb07b4600dea512174cf87cdcc1e590a7
unknown <eexdhe@gmail.com>
2021-03-03 13:42:55 +0100

    create route to update todo

--- src/server/routers/todo.js
+++ src/server/routers/todo.js
@@ -35,18 +35,46 @@ router.delete("/todo/:id", auth, async (req, res) => {
     return res.status(401).send({ error: "You need to be logged" });
   }
 
-
   const { params } = req;
-  try{
-    const todo = await Todo.findOneAndDelete({_id: params.id, owner: req.user._id});
-    if(!todo){
-      return res.status(404).send({error: 'Todo doesn\'t exist'});
+  try {
+    const todo = await Todo.findOneAndDelete({
+      _id: params.id,
+      owner: req.user._id,
+    });
+    if (!todo) {
+      return res.status(404).send({ error: "Todo doesn't exist" });
     }
     res.status(200).send(todo);
-  } catch(e){
-    res.status(500).send({error: e.message});
+  } catch (e) {
+    res.status(500).send({ error: e.message });
+  }
+});
+
+router.patch("/todo/:id", auth, async (req, res) => {
+  if (!req.user) {
+    return res.status(401).send({ error: "You need to be logged" });
   }
 
+  const updates = req.body;
+  const updatesKeys = Object.keys(updates)
+  const allowedUpdates = ['completed', 'description'];
+
+  // check if the updates are among the allowed ones
+  const isAllowed = updatesKeys.every((update) => allowedUpdates.includes(update));
+
+  if(!isAllowed){
+    return res.send({error: "updates are not valid"});
+  }
+
+  const todo = await Todo.findOne({_id: req.params.id, owner: req.user._id});
+
+  updatesKeys.forEach((key) => {
+    todo[key] = updates[key];
+  });
+
+  await todo.save();
+
+  res.send(todo);
 });
 
 module.exports = router;
