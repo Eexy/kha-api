52ce5678dd58c3f4cf66fa74d02f68df91b94598
unknown <eexdhe@gmail.com>
2021-04-06 16:02:59 +0200

    add query to get todos

--- src/server/routers/todo.js
+++ src/server/routers/todo.js
@@ -4,13 +4,32 @@ const Todo = require("../../db/models/todo");
 const User = require("../../db/models/user");
 const router = express.Router();
 
+// GET /tasks?completed=true
+// GET /tasks?sortBy=createdAt:desc
+// GET /tasks?limit=10
 router.get("/todos", auth, async (req, res) => {
   if (!req.user) {
     return res.status(401).send({ error: "You need to be logged" });
   }
 
   try {
-    await req.user.populate({ path: "todos" }).execPopulate();
+    const match = {};
+    const sort = {};
+
+    if (req.query.completed) {
+      match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+      const parts = req.query.sortBy.split(":");
+      sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+
+    await req.user.populate({ path: "todos", match, options: {
+      limit: parseInt(req.query.limit),
+      sort
+    } }).execPopulate();
+
     res.status(200).send(req.user.todos);
   } catch (e) {
     res.status(500).send({ error: e.message });
@@ -56,17 +75,19 @@ router.patch("/todo/:id", auth, async (req, res) => {
   }
 
   const updates = req.body;
-  const updatesKeys = Object.keys(updates)
-  const allowedUpdates = ['completed', 'description'];
+  const updatesKeys = Object.keys(updates);
+  const allowedUpdates = ["completed", "description"];
 
   // check if the updates are among the allowed ones
-  const isAllowed = updatesKeys.every((update) => allowedUpdates.includes(update));
+  const isAllowed = updatesKeys.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-  if(!isAllowed){
-    return res.send({error: "updates are not valid"});
+  if (!isAllowed) {
+    return res.send({ error: "updates are not valid" });
   }
 
-  const todo = await Todo.findOne({_id: req.params.id, owner: req.user._id});
+  const todo = await Todo.findOne({ _id: req.params.id, owner: req.user._id });
 
   updatesKeys.forEach((key) => {
     todo[key] = updates[key];
