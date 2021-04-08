2c134421ec7d841137a242622850b4be11b628b6
unknown <eexdhe@gmail.com>
2021-04-08 16:48:07 +0200

    send token when user logged

--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -8,8 +8,7 @@ router.post("/users/login", async (req, res) => {
   try{
     const user = await User.findByCredential(req.body.email, req.body.password);
     const token = await user.generateJWT();
-    res.cookie('jwt', token, {httpOnly: true});
-    res.status(200).send({message: "login successful"});
+    res.status(200).send({token});
   } catch(e){
     res.status(404).send({error: e.message});
   }
