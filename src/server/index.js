f597db3ae44a036e0daca458ef744f3e9eb1657f
unknown <eexdhe@gmail.com>
2021-04-10 16:06:21 +0200

    install cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const userRouter = require('./routers/user');
 const todoRouter = require('./routers/todo');
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ require('../db/db');
 // configure express
 app.use(helmet());
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
